e2437e49cfbde3c2ded216c9e650f59557fa83e5
haekyu.cho <haekyu.cho@nhn-commerce.com>
2025-03-18 23:27:07 +0900

    customTkinter example

--- src/multi_crawler/gui/test.py
+++ src/multi_crawler/gui/test.py
@@ -38,83 +38,87 @@ class App(CTk):
         self.geometry(f"{1200}x{800}")
 
         # configure grid layout (2x3)
-        self.grid_columnconfigure(0, weight=1)
-        self.grid_columnconfigure(1, weight=4)
+        self.grid_columnconfigure((1), weight=1)
         self.grid_rowconfigure((0, 1, 2), weight=1)
 
-        # 설정 영역 frame
-        self.config_frame = CTkFrame(self)
-        self.config_frame.grid_columnconfigure((0, 1), weight=1)
-        self.config_frame.grid(row=0, column=0, padx=0, pady=0, sticky="w")
-
-        self.label_config_group = CTkLabel(
-            master=self.config_frame, text="*Configuration*"
+        # 왼쪽-상단 Frame
+        self.left_top_frame = CTkFrame(self, corner_radius=0)
+        self.left_top_frame.grid(
+            row=0, column=0, padx=(20, 0), pady=(20, 0), sticky="nwe"
         )
-        self.label_config_group.grid(row=0, column=0, columnspan=1, padx=10, pady=10)
 
-        # 추출 버튼 frame
-        self.extract_frame = CTkFrame(self)
-        self.extract_frame.grid(row=1, column=0, padx=0, pady=0, sticky="nsew")
-
-        self.extract_agency = CTkButton(
-            self.extract_frame, text="에이전시 정보 크롤링 시작", width=300
+        self.left_top_frame.grid_columnconfigure((0, 1), weight=1)
+        self.left_top_frame.grid_rowconfigure((1, 2), weight=1)
+        self.config_label = CTkLabel(
+            self.left_top_frame,
+            text="Configuration",
+            font=CTkFont(size=20, weight="bold"),
         )
-        self.extract_agency.grid(row=0, column=0, padx=0, pady=0, sticky="")
 
-        self.extract_skin = CTkButton(
-            self.extract_frame, text="스킨 정보 크롤링 시작", width=300
+        self.config_label.grid(row=0, column=0, columnspan=2, padx=0, pady=10)
+
+        switch = CTkSwitch(master=self.left_top_frame, text="headless")
+        switch.grid(row=1, column=0, padx=(50, 0), pady=(20, 0), sticky="w")
+
+        self.slider_1 = CTkSlider(self.left_top_frame, from_=0, to=1, number_of_steps=5)
+        self.slider_1.grid(row=2, column=0, padx=(40, 40), pady=(20, 20), sticky="w")
+
+        # # 왼쪽-중단 Frame
+        self.left_middle_frame = CTkFrame(self, corner_radius=0)
+        self.left_middle_frame.grid(
+            row=1, column=0, padx=(20, 0), pady=(20, 0), sticky="ew"
         )
-        self.extract_skin.grid(row=1, column=0, padx=0, pady=0, sticky="")
+        self.left_middle_frame.grid_rowconfigure((0, 1, 2, 4), weight=1)
 
-        # 엑셀 버튼 frame
-        self.excel_frame = CTkFrame(self)
-        self.excel_frame.grid(row=2, column=0, padx=0, pady=0, sticky="nsew")
-
-        self.excel_agency = CTkButton(
-            self.excel_frame, text="에이전시 정보 크롤링 시작", width=300
+        self.saved_label = CTkLabel(
+            self.left_middle_frame,
+            text="Saved list",
+            font=CTkFont(size=20, weight="bold"),
         )
-        self.excel_agency.grid(row=0, column=0, padx=0, pady=0, sticky="")
 
-        self.excel_skin = CTkButton(
-            self.excel_frame, text="스킨 정보 크롤링 시작", width=300
+        self.saved_label.grid(row=0, column=0, padx=0, pady=0)
+
+        self.skin_entry = CTkEntry(
+            self.left_middle_frame,
+            textvariable=tkinter.StringVar(value="skin list - 3 count"),
+            state="readonly",
         )
-        self.excel_skin.grid(row=1, column=0, padx=0, pady=0, sticky="")
+        self.skin_entry.grid(row=1, column=0, padx=(20, 0), pady=(20, 0), sticky="ew")
 
-        # 오른쪽 로그
-        # create textbox
-        self.textbox = CTkTextbox(self, width=600)
+        self.agency_entry = CTkEntry(
+            self.left_middle_frame,
+            textvariable=tkinter.StringVar(value="agency list - 360 count"),
+            state="readonly",
+        )
+        self.agency_entry.grid(row=2, column=0, padx=(20, 0), pady=(20, 0), sticky="ew")
+
+        self.agency_entry = CTkEntry(
+            self.left_middle_frame,
+            textvariable=tkinter.StringVar(value="apps list - 360 count"),
+            state="readonly",
+        )
+        self.agency_entry.grid(row=3, column=0, padx=(20, 0), pady=(20, 0), sticky="ew")
+
+        # 왼쪽-하단 Frame
+        self.left_bottom_frame = CTkFrame(self, width=140, corner_radius=0)
+        self.left_bottom_frame.grid(
+            row=2, column=0, padx=(20, 0), pady=(20, 0), sticky="nsew"
+        )
+        self.left_middle_frame.grid_rowconfigure(2, weight=1)
+
+        self.download_label = CTkLabel(
+            self.left_bottom_frame,
+            text="Saved list",
+            font=CTkFont(size=20, weight="bold"),
+        )
+
+        self.download_label.grid(row=0, column=0, padx=0, pady=0)
+
+        # 오른쪽 Textbox
+        self.textbox = CTkTextbox(master=self, width=250)
         self.textbox.grid(
             row=0, column=1, rowspan=3, padx=(20, 0), pady=(20, 0), sticky="nsew"
         )
-
-        # self.radio_var = tkinter.IntVar(value=0)
-        # self.label_radio_group = CTkLabel(
-        #     master=self.radiobutton_frame, text="CTkRadioButton Group:"
-        # )
-        # self.label_radio_group.grid(
-        #     row=0, column=2, columnspan=1, padx=10, pady=10, sticky=""
-        # )
-        # self.radio_button_1 = CTkRadioButton(
-        #     master=self.radiobutton_frame, variable=self.radio_var, value=0
-        # )
-        # self.radio_button_1.grid(row=1, column=2, pady=10, padx=20, sticky="n")
-        # self.radio_button_2 = CTkRadioButton(
-        #     master=self.radiobutton_frame, variable=self.radio_var, value=1
-        # )
-        # self.radio_button_2.grid(row=2, column=2, pady=10, padx=20, sticky="n")
-        # self.radio_button_3 = CTkRadioButton(
-        #     master=self.radiobutton_frame, variable=self.radio_var, value=2
-        # )
-        # self.radio_button_3.grid(row=3, column=2, pady=10, padx=20, sticky="n")
-
-        self.textbox.insert(
-            "0.0",
-            "크롤링을 시작합니다.\n\n"
-            + "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.\n\n"
-            * 20,
-        )
-
-        self.textbox.insert(index=10.0, text="야 서강준\n서장준")
 
 
 if __name__ == "__main__":
